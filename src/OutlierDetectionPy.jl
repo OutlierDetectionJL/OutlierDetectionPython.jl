6785a64016cc138ea58e77f2113690d85c744659
David Muhr <muhrdavid+github@gmail.com>
2021-09-16 14:26:46 +0200

    fix model exports

--- src/OutlierDetectionPy.jl
+++ src/OutlierDetectionPy.jl
@@ -1,35 +1,36 @@
 module OutlierDetectionPy
     using OutlierDetectionInterface
     const OD = OutlierDetectionInterface
-
+    
     include("utils.jl")
     include("models.jl")
 
-    MODELS = (PyABODDetector,
-              PyCBLOFDetector,
-              PyCOFDetector,
-              PyCOPODDetector,
-              PyHBOSDetector,
-              PyIForestDetector,
-              PyKNNDetector,
-              PyLMDDDetector,
-              PyLODADetector,
-              PyLOFDetector,
-              PyLOCIDetector,
-              PyMCDDetector,
-              PyOCSVMDetector,
-              PyPCADetector,
-              PyRODDetector,
-              PySODDetector,
-              PySOSDetector)
+    UUID = "2449c660-d36c-460e-a68b-92ab3c865b3e"
+    MODELS = [:PyABODDetector,
+              :PyCBLOFDetector,
+              :PyCOFDetector,
+              :PyCOPODDetector,
+              :PyHBOSDetector,
+              :PyIForestDetector,
+              :PyKNNDetector,
+              :PyLMDDDetector,
+              :PyLODADetector,
+              :PyLOFDetector,
+              :PyLOCIDetector,
+              :PyMCDDetector,
+              :PyOCSVMDetector,
+              :PyPCADetector,
+              :PyRODDetector,
+              :PySODDetector,
+              :PySOSDetector]
 
-    ORG = "OutlierDetectionJL"
-    UUID = "51249a0a-cb36-4849-8e04-30c7f8d311bb"
     for model in MODELS
-        @eval(export $model)
-        OD.metadata_pkg(model, package_name=@__MODULE__, package_uuid=UUID,
-                            package_url="https://github.com/$ORG/$(@__MODULE__).jl",
-                            is_pure_julia=true, package_license="MIT", is_wrapper=false)
-        OD.load_path(::Type{model}) = "$(@__MODULE__).$model"
+        @eval begin
+            OD.metadata_pkg($model, package_name=string(@__MODULE__), package_uuid=$UUID,
+                            package_url="https://github.com/OutlierDetectionJL/$(@__MODULE__).jl",
+                            is_pure_julia=false, package_license="MIT", is_wrapper=false)
+            OD.load_path(::Type{$model}) = string($model)
+            export $model
+        end
     end
 end
